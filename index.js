d3d954296866706a8740bfd1d850727f0259850c
miriam590 <miriam.yego@student.moringaschool.com>
2025-01-09 15:41:11 +0300

    index.js

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+// isPalindrome.js
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+function isPalindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+  // Compare the cleaned string with its reverse
+  return cleanedStr === cleanedStr.split('').reverse().join('');
 }
 
 module.exports = isPalindrome;
